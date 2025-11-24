9c165763d7ad47fd3d09924f7ac537105dc9188d
Lucas Gades <70823238+Lucas-Gades@users.noreply.github.com>
2025-11-24 10:08:23 -0300

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "cadastro-produto-senacplace", appId: "1:560071899984:web:fd9cc75d8a110c14c50a55", storageBucket: "cadastro-produto-senacplace.firebasestorage.app", apiKey: "AIzaSyBN65cenTgN02N2NNsWpf7cBMUPlGZYecI", authDomain: "cadastro-produto-senacplace.firebaseapp.com", messagingSenderId: "560071899984"})), provideFirestore(() => getFirestore())
   ]
 };
